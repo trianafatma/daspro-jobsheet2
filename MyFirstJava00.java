c1927ea02aad5d2fe18b0e76b9c51749daeff239
unknown <triana.f@gmail.com>
2023-09-05 07:42:53 +0700

    commit MyFirstJava00.java

--- MyFirstJava00.java
+++ MyFirstJava00.java
@@ -1,5 +1,5 @@
 public class MyFirstJava00 {
     public static void main (String[] args){
-        System.out.println("Hello, saya mhs JTI Polinema");
+        System.out.println("Hello, saya mahasiswa JTI Polinema");
     }
 }
